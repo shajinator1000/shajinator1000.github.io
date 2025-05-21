205da45ea27d09dd228b6cf45caeb5af1f973da8
shajinator1000 <shajeepan21@gmail.com>
2025-05-21 20:49:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Script loaded");
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks for joining the OmniExams waitlist!");
+});
